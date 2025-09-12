26bda6bd68545a2044c00f8ba5ff304bda694c1c
Vashi xyz <mvashista3@gmail.com>
2025-09-12 13:09:59 +0530

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -24,7 +24,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 const passwords: Record<string, string> = {
   admin: "admin#cc",
-  spot: "spot@cc",
+  spot: "spot#cc",
   entry: "entry@cc",
 };
 
